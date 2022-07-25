032c48397ef9864ed672ed4b6adf3191864eead8
b083040012@g-mail.nsysu.edu.tw <b083040012@g-mail.nsysu.edu.tw>
2022-07-25 17:06:34 +0800

    for functional model

--- retrain_architecture.py
+++ retrain_architecture.py
@@ -2,7 +2,7 @@ import keras, logging, yaml, time
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
-from models import STDN_Network
+from choice_block_model import SAGAN_Functional_Model
 from sagan_file_loader import SAGAN_fileloader
 from criterion import eval_together, eval_lstm
 config = tf.compat.v1.ConfigProto()
@@ -64,7 +64,20 @@ def retrain_architecture(batch_size=64, max_epochs=100, validation_split=0.2, ea
                                                                                               short_term_lstm_seq_len, hist_feature_daynum,\
                                                                                               last_feature_num)
 
-    train_data = [att_cnn, att_flow, att_lstm, att_weather, short_cnn, short_flow, [short_lstm, ], short_weather, short_poi]
+    print("size of training data: ")
+    print("att_cnn: ", len(att_cnn), att_cnn[0].shape)
+    print("att_flow: ", len(att_flow), att_flow[0].shape)
+    print("att_lstm: ", len(att_lstm), att_lstm[0].shape)
+    print("att_weather: ", len(att_weather), att_weather[0].shape)
+    # print("att poi: ", len(att_poi), att_poi[0].shape)
+    print("short_cnn: ", len(short_cnn), short_cnn[0].shape)
+    print("short_flow: ", len(short_flow), short_flow[0].shape)
+    print("short_lstm: ", short_lstm.shape)
+    print("short_weather: ", short_weather.shape)
+    print("short poi: ", short_poi.shape)
+    print("y: ", y.shape)
+
+    # train_data = [att_cnn, att_flow, att_lstm, att_weather, short_cnn, short_flow, [short_lstm, ], short_weather, short_poi]
     train_label = y
     print("Start training supernet with input shape {1} / {0}".format(short_lstm.shape, short_cnn[0].shape))
     logging.info("train data loading complete")
@@ -74,19 +87,20 @@ def retrain_architecture(batch_size=64, max_epochs=100, validation_split=0.2, ea
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, \
                             save_best_only=True, save_weights_only=True, \
                             mode='auto')
-    tf.config.run_functions_eagerly(True)
+    # tf.config.run_functions_eagerly(True)
     searched_choice=np.load(open(config["file"]["path"]+"searched_choice_list.npy", "rb"), allow_pickle = True)
-    model=STDN_Network(searched_choice, att_lstm_num=att_lstm_num, att_lstm_seq_len=long_term_lstm_seq_len, \
-                            lstm_seq_len=len(short_cnn), feature_vec_len=short_lstm.shape[-1], \
-                            cnn_flat_size=cnn_flat_size, nbhd_size=short_cnn[0].shape[1], nbhd_type=short_cnn[0].shape[-1])
-    logging.info("architecture loading complete")
 
-    model.compile(optimizer = 'adagrad', loss = 'mse', metrics=[])
+    modeler = SAGAN_Functional_Model()
+    model = modeler.func_model(nas_choice = searched_choice, att_lstm_num = att_lstm_num, att_lstm_seq_len = long_term_lstm_seq_len, lstm_seq_len = short_term_lstm_seq_len, \
+        feature_vec_len = short_lstm.shape[-1], cnn_flat_size = cnn_flat_size, lstm_out_size = 128, nbhd_size = short_cnn[0].shape[1], poi_size = short_poi.shape[1], \
+        nbhd_type = 2, flow_type = short_flow[0].shape[-1], weather_type = short_weather.shape[-1], poi_type = short_poi.shape[-1], output_shape = 2, optimizer = 'adagrad', loss = 'mse', metrics=[])
+
+    logging.info("architecture loading complete")
 
     logging.info("retraining start")
     start=time.time()
     model.fit( \
-        x = train_data, \
+        x = att_cnn + att_flow + att_lstm + att_weather + short_cnn + short_flow + [short_lstm,] + [short_weather,] + [short_poi,], \
         y = train_label, \
         batch_size=batch_size, validation_split=validation_split, epochs=max_epochs, callbacks=[early_stop, checkpoint])
     end=time.time()
@@ -94,6 +108,8 @@ def retrain_architecture(batch_size=64, max_epochs=100, validation_split=0.2, ea
 
     model.save_weights(config["file"]["path"]+"retrained_final_weights")
     logging.info("[Retrained Architecture Weight Saved]")
+    model.save(config["file"]["path"]+"retrained_final_model.hdf5")
+    logging.info("[Retrained Architecture Model Saved]")
     logging.info("[Retraining Architecture Phase End] : total time: {0} sec".format(end-start))
 
 if __name__=='__main__':
